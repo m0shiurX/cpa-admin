d00a6c2e1b1a6a30bcb939a93bdb35ccb597839f
Moshiur Rahman <unimrgm@gmail.com>
2022-04-10 12:45:06 +0600

    🛠️ Footer updated

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { Head, Link } from "@inertiajs/inertia-vue3";
-import Logo from "../Components/Logo.vue";
+import { Head, Link } from '@inertiajs/inertia-vue3';
+import Logo from '../Components/Logo.vue';
 defineProps({
     canLogin: Boolean,
     canRegister: Boolean,
@@ -24,45 +24,22 @@ defineProps({
                     class="px-2) bg-gradient-to-t from-green-200 via-white px-2 uppercase hover:bg-gradient-to-t hover:from-green-200 hover:via-white"
                     >Home</a
                 >
-                <a
-                    href="#"
-                    class="px-2) uppercase hover:bg-gradient-to-t hover:from-green-200 hover:via-white"
-                    >Services</a
-                >
-                <a
-                    href="#"
-                    class="px-2) uppercase hover:bg-gradient-to-t hover:from-green-200 hover:via-white"
-                    >About</a
-                >
-                <a
-                    href="#"
-                    class="px-2) uppercase hover:bg-gradient-to-t hover:from-green-200 hover:via-white"
-                    >Contact</a
-                >
-                <a
-                    href="#"
-                    class="rounded-lg border border-sky-600 py-3 px-6 font-semibold uppercase text-sky-600 hover:bg-sky-600 hover:text-white"
+                <a href="#" class="px-2) uppercase hover:bg-gradient-to-t hover:from-green-200 hover:via-white">Services</a>
+                <a href="#" class="px-2) uppercase hover:bg-gradient-to-t hover:from-green-200 hover:via-white">About</a>
+                <a href="#" class="px-2) uppercase hover:bg-gradient-to-t hover:from-green-200 hover:via-white">Contact</a>
+                <a href="#" class="rounded-lg border border-sky-600 py-3 px-6 font-semibold uppercase text-sky-600 hover:bg-sky-600 hover:text-white"
                     >Start a Project</a
                 >
             </div>
         </nav>
         <!-- hero content -->
-        <div
-            id="hero__content"
-            class="mx-auto mt-56 flex max-w-3xl flex-col items-center space-y-4 text-center"
-        >
+        <div id="hero__content" class="mx-auto mt-56 flex max-w-3xl flex-col items-center space-y-4 text-center">
             <h1 class="text-5xl font-bold leading-snug text-gray-800">
                 We make remarkable <br />
                 digital products
             </h1>
-            <p class="text-base text-gray-500">
-                We design and build beautiful websites, apps and branding
-            </p>
-            <Link
-                v-if="canRegister"
-                :href="route('register')"
-                class="inline-block rounded-lg bg-green-400 px-6 py-3 uppercase text-white"
-            >
+            <p class="text-base text-gray-500">We design and build beautiful websites, apps and branding</p>
+            <Link v-if="canRegister" :href="route('register')" class="inline-block rounded-lg bg-green-400 px-6 py-3 uppercase text-white">
                 Join our Network
             </Link>
         </div>
@@ -71,9 +48,7 @@ defineProps({
     <!-- Clients/Brands -->
     <div class="min-h-40 bg-sky-500 py-20">
         <section class="container max-w-screen-xl text-center">
-            <h3 class="text-gray-200">
-                Trusted by <span class="text-gray-50">35+</span> biggest brands
-            </h3>
+            <h3 class="text-gray-200">Trusted by <span class="text-gray-50">35+</span> biggest brands</h3>
             <div class="mt-20 flex justify-around gap-5">
                 <div>
                     <dt class="sr-only">logo of Adverus</dt>
@@ -116,47 +91,26 @@ defineProps({
             What we do
         </h3>
 
-        <div
-            class="flex w-full flex-col items-end justify-between space-y-10 md:flex-row"
-        >
-            <h2
-                class="mt-5 text-5xl font-semibold leading-normal text-gray-800"
-            >
-                Delivering quality products
-            </h2>
+        <div class="flex w-full flex-col items-end justify-between space-y-10 md:flex-row">
+            <h2 class="mt-5 text-5xl font-semibold leading-normal text-gray-800">Delivering quality products</h2>
             <div class="flex max-w-screen-sm space-x-5">
                 <div class="flex w-1/2 items-center">
                     <span class="text-6xl font-medium text-green-500">13</span>
-                    <span class="w-1/2 px-2 normal-case"
-                        >Years in business</span
-                    >
+                    <span class="w-1/2 px-2 normal-case">Years in business</span>
                 </div>
                 <div class="flex w-1/2 items-center">
                     <span class="text-6xl font-medium text-green-500">28</span>
-                    <span class="w-1/2 px-2 normal-case"
-                        >Designers & developers</span
-                    >
+                    <span class="w-1/2 px-2 normal-case">Designers & developers</span>
                 </div>
             </div>
         </div>
 
-        <div
-            class="mt-12 flex flex-col space-y-8 md:mt-32 md:flex-row md:gap-x-10 md:space-y-0"
-        >
-            <div
-                class="group rounded-lg bg-white px-8 pt-16 pb-8 shadow-sm hover:shadow-xl md:w-1/3"
-            >
-                <img
-                    class="mb-16"
-                    src="@/assets/instagram.png"
-                    alt="instagram"
-                />
-                <h4 class="text-2xl font-semibold text-gray-700">
-                    CPA/Affiliate
-                </h4>
+        <div class="mt-12 flex flex-col space-y-8 md:mt-32 md:flex-row md:gap-x-10 md:space-y-0">
+            <div class="group rounded-lg bg-white px-8 pt-16 pb-8 shadow-sm hover:shadow-xl md:w-1/3">
+                <img class="mb-16" src="@/assets/instagram.png" alt="instagram" />
+                <h4 class="text-2xl font-semibold text-gray-700">CPA/Affiliate</h4>
                 <p class="mt-4 font-light text-gray-500">
-                    Eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis laboris nisi
+                    Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis laboris nisi
                 </p>
                 <a class="mt-8 inline-block" href="#">
                     <svg
@@ -166,25 +120,15 @@ defineProps({
                         viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg"
                     >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M9 5l7 7-7 7"
-                        ></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
                     </svg>
                 </a>
             </div>
-            <div
-                class="group rounded-lg bg-white px-8 pt-16 pb-8 shadow-sm hover:shadow-xl md:w-1/3"
-            >
+            <div class="group rounded-lg bg-white px-8 pt-16 pb-8 shadow-sm hover:shadow-xl md:w-1/3">
                 <img class="mb-16" src="@/assets/web.png" alt="instagram" />
-                <h4 class="text-2xl font-semibold text-gray-700">
-                    Web Design & Development
-                </h4>
+                <h4 class="text-2xl font-semibold text-gray-700">Web Design & Development</h4>
                 <p class="mt-4 font-light text-gray-500">
-                    Eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis laboris nisi
+                    Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis laboris nisi
                 </p>
                 <a class="mt-8 inline-block" href="#">
                     <svg
@@ -194,29 +138,15 @@ defineProps({
                         viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg"
                     >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M9 5l7 7-7 7"
-                        ></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
                     </svg>
                 </a>
             </div>
-            <div
-                class="group rounded-lg bg-white px-8 pt-16 pb-8 shadow-sm hover:shadow-xl md:w-1/3"
-            >
-                <img
-                    class="mb-16"
-                    src="@/assets/graphic-designer.png"
-                    alt="instagram"
-                />
-                <h4 class="text-2xl font-semibold text-gray-700">
-                    Graphics Design
-                </h4>
+            <div class="group rounded-lg bg-white px-8 pt-16 pb-8 shadow-sm hover:shadow-xl md:w-1/3">
+                <img class="mb-16" src="@/assets/graphic-designer.png" alt="instagram" />
+                <h4 class="text-2xl font-semibold text-gray-700">Graphics Design</h4>
                 <p class="mt-4 font-light text-gray-500">
-                    Eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis laboris nisi
+                    Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis laboris nisi
                 </p>
                 <a class="mt-8 inline-block" href="#">
                     <svg
@@ -226,33 +156,17 @@ defineProps({
                         viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg"
                     >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M9 5l7 7-7 7"
-                        ></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
                     </svg>
                 </a>
             </div>
         </div>
-        <div
-            class="mt-4 flex flex-col space-y-8 md:mt-6 md:flex-row md:gap-x-10 md:space-y-0"
-        >
-            <div
-                class="group rounded-lg bg-white px-8 pt-16 pb-8 shadow-sm hover:shadow-xl md:w-1/3"
-            >
-                <img
-                    class="mb-16"
-                    src="@/assets/instagram.png"
-                    alt="instagram"
-                />
-                <h4 class="text-2xl font-semibold text-gray-700">
-                    Digital Marketing
-                </h4>
+        <div class="mt-4 flex flex-col space-y-8 md:mt-6 md:flex-row md:gap-x-10 md:space-y-0">
+            <div class="group rounded-lg bg-white px-8 pt-16 pb-8 shadow-sm hover:shadow-xl md:w-1/3">
+                <img class="mb-16" src="@/assets/instagram.png" alt="instagram" />
+                <h4 class="text-2xl font-semibold text-gray-700">Digital Marketing</h4>
                 <p class="mt-4 font-light text-gray-500">
-                    Eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis laboris nisi
+                    Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis laboris nisi
                 </p>
                 <a class="mt-8 inline-block" href="#">
                     <svg
@@ -262,25 +176,15 @@ defineProps({
                         viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg"
                     >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M9 5l7 7-7 7"
-                        ></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
                     </svg>
                 </a>
             </div>
-            <div
-                class="group rounded-lg bg-white px-8 pt-16 pb-8 shadow-sm hover:shadow-xl md:w-1/3"
-            >
+            <div class="group rounded-lg bg-white px-8 pt-16 pb-8 shadow-sm hover:shadow-xl md:w-1/3">
                 <img class="mb-16" src="@/assets/web.png" alt="instagram" />
-                <h4 class="text-2xl font-semibold text-gray-700">
-                    App Development
-                </h4>
+                <h4 class="text-2xl font-semibold text-gray-700">App Development</h4>
                 <p class="mt-4 font-light text-gray-500">
-                    Eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis laboris nisi
+                    Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis laboris nisi
                 </p>
                 <a class="mt-8 inline-block" href="#">
                     <svg
@@ -290,29 +194,15 @@ defineProps({
                         viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg"
                     >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M9 5l7 7-7 7"
-                        ></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
                     </svg>
                 </a>
             </div>
-            <div
-                class="group rounded-lg bg-white px-8 pt-16 pb-8 shadow-sm hover:shadow-xl md:w-1/3"
-            >
-                <img
-                    class="mb-16"
-                    src="@/assets/graphic-designer.png"
-                    alt="instagram"
-                />
-                <h4 class="text-2xl font-semibold text-gray-700">
-                    Social Media Management
-                </h4>
+            <div class="group rounded-lg bg-white px-8 pt-16 pb-8 shadow-sm hover:shadow-xl md:w-1/3">
+                <img class="mb-16" src="@/assets/graphic-designer.png" alt="instagram" />
+                <h4 class="text-2xl font-semibold text-gray-700">Social Media Management</h4>
                 <p class="mt-4 font-light text-gray-500">
-                    Eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis laboris nisi
+                    Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis laboris nisi
                 </p>
                 <a class="mt-8 inline-block" href="#">
                     <svg
@@ -322,12 +212,7 @@ defineProps({
                         viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg"
                     >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M9 5l7 7-7 7"
-                        ></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
                     </svg>
                 </a>
             </div>
@@ -337,9 +222,7 @@ defineProps({
     </section>
 
     <section class="container max-w-screen-xl py-20">
-        <div
-            class="flex flex-col items-center justify-between gap-y-10 gap-x-10 xl:flex-row"
-        >
+        <div class="flex flex-col items-center justify-between gap-y-10 gap-x-10 xl:flex-row">
             <div class="rounded-full border-2 p-8">
                 <Logo class="h-96 w-96 object-cover p-5" />
             </div>
@@ -349,16 +232,10 @@ defineProps({
                 >
                     Why work with us?
                 </h3>
-                <h2
-                    class="mt-5 text-5xl font-semibold leading-normal text-gray-800"
-                >
-                    Profitable and fast!
-                </h2>
+                <h2 class="mt-5 text-5xl font-semibold leading-normal text-gray-800">Profitable and fast!</h2>
                 <p class="my-8 text-base font-normal text-gray-500">
-                    We are giving the highest payout in the country and We have
-                    fast payment system. We have wise managers, Who will serve
-                    you all the time. We invite you. Join with us, and be more
-                    profitable.
+                    We are giving the highest payout in the country and We have fast payment system. We have wise managers, Who will serve you all the
+                    time. We invite you. Join with us, and be more profitable.
                 </p>
                 <Link
                     class="inline-block rounded-lg bg-green-400 px-9 py-4 text-sm uppercase text-white"
@@ -373,22 +250,15 @@ defineProps({
     </section>
 
     <footer class="h-96 w-full bg-slate-700 py-20">
-        <div
-            class="container flex h-full flex-col items-start justify-start gap-y-10 gap-x-10 xl:flex-row"
-        >
-            <div class="h-full max-w-md border-r pr-16">
+        <div class="container flex flex-col items-start justify-start gap-y-10 gap-x-10 xl:flex-row">
+            <div class="h-full max-w-md border-r border-slate-500 pr-16">
                 <div class="mb-5 flex items-center gap-x-5">
                     <Logo class="fill-gray-100" />
-                    <span class="text-2xl text-gray-50">
-                        Mondol IT Limited
-                    </span>
+                    <span class="text-2xl text-gray-50"> Mondol IT Limited </span>
                 </div>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Repudiandae, a veritatis recusandae, minima excepturi
-                    perferendis cupiditate dolorum harum consequuntur ex quas
-                    temporibus quisquam iusto ea corrupti illum quaerat,
-                    architecto quo.
+                <p class="text-sm text-slate-400">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae, a veritatis recusandae, minima excepturi perferendis
+                    cupiditate dolorum harum consequuntur ex quas temporibus quisquam iusto ea corrupti illum quaerat, architecto quo.
                 </p>
             </div>
             <div class="flex h-full w-full gap-x-20 pl-6 text-slate-400">
@@ -413,13 +283,25 @@ defineProps({
                 </div>
                 <div>
                     <h2 class="mb-3 text-xl text-white">Find Us</h2>
-                    <p>Greenwich Village & Soho, 10012 New York, USA</p>
+                    <p>
+                        Greenwich Village & Soho, 10012 <br />
+                        New York, USA
+                    </p>
                     <p>
                         800 123-456 <br />
                         digity@example.com
                     </p>
                 </div>
             </div>
+        </div>
+        <div class="container mt-10">
+            <hr class="border-slate-500" />
+        </div>
+        <div class="container mt-2 text-slate-400">
+            <p class="text-sm">
+                All rights reserved by MONDOL IT LIMITED
+                <span class="">|</span> Copyright &copy; 2022
+            </p>
         </div>
     </footer>
 </template>
